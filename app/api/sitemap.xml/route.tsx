3eead8495bef090227056f494192b81752c2dad6
abiudev <bdmasinde@gmail.com>
2025-03-18 12:45:31 +0300

    sitemap updated

--- app/api/sitemap.xml/route.tsx
+++ app/api/sitemap.xml/route.tsx
@@ -3,42 +3,73 @@ import { NextResponse } from "next/server";
 export async function GET() {
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        <url>
-            <loc>https://www.ajmedia.co.ke/</loc>
-            <lastmod>2025-01-30T08:38:36+01:00</lastmod>
-            <priority>0.2</priority>
-        </url>
-        <url>
-            <loc>https://www.ajmedia.co.ke/Contact</loc>
-            <lastmod>2025-01-30T08:38:36+01:00</lastmod>
-            <priority>0.1</priority>
-        </url>
-        <url>
-            <loc>https://www.ajmedia.co.ke/Services/photography</loc>
-            <lastmod>2025-01-30T08:38:36+01:00</lastmod>
-            <priority>0.1</priority>
-        </url>
-        <url>
-            <loc>https://www.ajmedia.co.ke/Services/videography</loc>
-            <lastmod>2025-01-30T08:38:36+01:00</lastmod>
-            <priority>0.1</priority>
-        </url>
-        <url>
-            <loc>https://www.ajmedia.co.ke/Services/corporate</loc>
-            <lastmod>2025-01-30T08:38:36+01:00</lastmod>
-            <priority>0.1</priority>
-        </url>
-        <url>
-            <loc>https://www.ajmedia.co.ke/Services/webdev</loc>
-            <lastmod>2025-01-30T08:38:36+01:00</lastmod>
-            <priority>0.1</priority>
-        </url>
-        <url>
-            <loc>https://www.ajmedia.co.ke/Services/rapporteur</loc>
-            <lastmod>2025-01-30T08:38:36+01:00</lastmod>
-            <priority>0.1</priority>
-        </url>
-    </urlset>`;
+<url>
+<loc>https://ajmedia.co.ke/</loc>
+<lastmod>2025-03-18</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+<url>
+<loc>https://www.ajmedia.co.ke/Services/digital-marketing</loc>
+<lastmod>2025-03-18</lastmod>
+<changefreq>weekly</changefreq>
+<priority>0.9</priority>
+</url>
+<url>
+<loc>https://ajmedia.co.ke/about</loc>
+<lastmod>2025-03-18</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.8</priority>
+</url>
+<url>
+<loc>https://ajmedia.co.ke/services</loc>
+<lastmod>2025-03-18</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.8</priority>
+</url>
+<url>
+<loc>https://ajmedia.co.ke/services/videography</loc>
+<lastmod>2025-03-18</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.7</priority>
+</url>
+<url>
+<loc>https://ajmedia.co.ke/services/photography</loc>
+<lastmod>2025-03-18</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.7</priority>
+</url>
+<url>
+<loc>https://ajmedia.co.ke/services/event-coverage</loc>
+<lastmod>2025-03-18</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.7</priority>
+</url>
+<url>
+<loc>https://ajmedia.co.ke/services/web-development</loc>
+<lastmod>2025-03-18</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.7</priority>
+</url>
+<url>
+<loc>https://ajmedia.co.ke/services/rapporteuring</loc>
+<lastmod>2025-03-18</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.7</priority>
+</url>
+<url>
+<loc>https://ajmedia.co.ke/portfolio</loc>
+<lastmod>2025-03-18</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.7</priority>
+</url>
+<url>
+<loc>https://ajmedia.co.ke/contact</loc>
+<lastmod>2025-03-18</lastmod>
+<changefreq>yearly</changefreq>
+<priority>0.6</priority>
+</url>
+</urlset>`;
 
   return new NextResponse(sitemap, {
     headers: {
